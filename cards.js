15fd5915f223ca239c26ec8144401fc56fcf0947
Daniella Galitsyna <galitsina@bstec.ru>
2023-01-26 17:56:44 +0100

    Add cursor:pointer on the card Fix long user’s name Fix opened image’s background Add adding popup in layout Fix submit forms Rename variables Аrray with cards moved to a separate file

---
+++ cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Гронинген',
+    link: 'https://images.unsplash.com/photo-1616321741705-e9a4073af35c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3120&q=80'
+  },
+  {
+    name: 'Волендам',
+    link: 'https://images.unsplash.com/photo-1609875103184-cba8296a5220?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
+  },
+  {
+    name: 'Амстердам',
+    link: 'https://images.unsplash.com/photo-1618333302170-d7bbc76188da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+  },
+  {
+    name: 'Доесбург',
+    link: 'https://images.unsplash.com/photo-1627496143655-285ccb0e938f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+  },
+  {
+    name: 'Амеланд',
+    link: 'https://images.unsplash.com/photo-1621583706700-eb7904e5d286?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+  },
+  {
+    name: 'Утрехт',
+    link: 'https://images.unsplash.com/photo-1656977711959-47776bfec276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1313&q=80'
+  }
+];
--- script.js
+++ script.js
@@ -1,116 +1,82 @@
-const editPopup = document.querySelector('.popup_edit-form');
-const addPopup = document.querySelector('.popup_add-form');
+const editingProfilePopup = document.querySelector('.popup_edit-form');
+const addingCardPopup = document.querySelector('.popup_add-form');
 const imagePopup = document.querySelector('.popup_open-image');
-const formElement = document.querySelector('.popup__container'); // Находим форму в DOM
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelectorAll('.popup__close-icon');
-const saveButton = formElement.querySelector('.popup__save-button');
+const closeButtons = document.querySelectorAll('.popup__close-icon');
 const addButton = document.querySelector('.profile__add-button');
-const popupHeading = formElement.querySelector('.popup__heading');
 const profileName = document.querySelector('.profile__name'); //имя профиля на странице
 const profileAbout = document.querySelector('.profile__bio'); //био профиля на странице
-const nameInput = formElement.querySelector('#name'); //1 поле редактирования в попапе
-const bioInput = formElement.querySelector('#about'); //2 поле редактирования в попапе
+const nameInput = editingProfilePopup.querySelector('#name'); //1 поле редактирования в попапе редактирования
+const bioInput = editingProfilePopup.querySelector('#about'); //2 поле редактирования в попапе редактирования
+const titleInput = addingCardPopup.querySelector('#title'); //1 поле редактирования в попапе создания
+const linkInput = addingCardPopup.querySelector('#link'); //2 поле редактирования в попапе создания
+const cardTemplate = document.querySelector('#element-template').content;
+const openedImage = imagePopup.querySelector('.popup__view-image');
+const openedCaption = imagePopup.querySelector('.popup__view-caption');
+const cardsContainer = document.querySelector('.elements'); //контейнер для всех карточек
 
-const initialCards = [
-  {
-    name: 'Гронинген',
-    link: 'https://images.unsplash.com/photo-1616321741705-e9a4073af35c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3120&q=80'
-  },
-  {
-    name: 'Волендам',
-    link: 'https://images.unsplash.com/photo-1609875103184-cba8296a5220?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80'
-  },
-  {
-    name: 'Амстердам',
-    link: 'https://images.unsplash.com/photo-1618333302170-d7bbc76188da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
-  },
-  {
-    name: 'Доесбург',
-    link: 'https://images.unsplash.com/photo-1627496143655-285ccb0e938f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
-  },
-  {
-    name: 'Амеланд',
-    link: 'https://images.unsplash.com/photo-1621583706700-eb7904e5d286?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
-  },
-  {
-    name: 'Утрехт',
-    link: 'https://images.unsplash.com/photo-1656977711959-47776bfec276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1313&q=80'
-  }
-];
-
+//функциональность открытия окон
 function openPopup(domElement) {
   domElement.classList.add('popup_opened');
 }
-
-//функциональность открытия окна редактирования
-function editProfile() {
-  openPopup(editPopup);
-  nameInput.value = profileName.textContent; //в первое поле записываем значение имени на странице
-  bioInput.value = profileAbout.textContent; //во второе поле записываем значение био на странице
-  nameInput.placeholder = 'Введите свое имя';
-  bioInput.placeholder = 'О себе';
-  popupHeading.textContent = 'Редактировать профиль';
-  saveButton.textContent = 'Сохранить';
-}
-editButton.addEventListener('click', editProfile);
-
-//функциональность открытия окна добавления карточки
-function addCard() {
-  openPopup(addPopup);
-  popupHeading.textContent = 'Новое место';
-  nameInput.placeholder = 'Название';
-  bioInput.placeholder = 'Ссылка на картинку';
-  saveButton.textContent = 'Создать';
-  nameInput.value = '';
-  bioInput.value = '';
-}
-addButton.addEventListener('click', addCard);
 
 //функциональность закрытия окон
 function closePopup(domElement) {
   domElement.classList.remove('popup_opened');
 }
 
-// Обработчик «отправки» формы, функциональность кнопок "Сохранить" и "Создать"
-function formSubmitHandler(evt) {
+//функциональность открытия окна редактирования
+function openEditProfile() {
+  openPopup(editingProfilePopup);
+  nameInput.value = profileName.textContent; //в первое поле записываем значение имени на странице
+  bioInput.value = profileAbout.textContent; //во второе поле записываем значение био на странице
+}
+editButton.addEventListener('click', openEditProfile);
+
+//функциональность открытия окна добавления карточки
+function openAddCard() {
+  openPopup(addingCardPopup);
+  titleInput.value = '';
+  linkInput.value = '';
+}
+addButton.addEventListener('click', openAddCard);
+
+// Обработчик «отправки» формы редактирования профиля
+function submitEditProfile(evt) {
   // Эта строчка отменяет стандартную отправку формы, так мы можем определить свою логику отправки.
   evt.preventDefault();
-  //условие для логики кнопки 'Cохранить', иначе для кнопки 'Создать'
-  if (saveButton.textContent === 'Сохранить') {
-    profileName.textContent = nameInput.value; //в значение имени на странице записываем значение из первого поля
-    profileAbout.textContent = bioInput.value; //в значение био на странице записываем значение из второго поля
-    closePopup(editPopup); //при нажатии на кнопку "сохранить" закрываем попап
-  } else {
-    //добавляем карточку в дерево при нажатии кнопки 'Создать'
-    const card = createCard(nameInput.value, bioInput.value);
-    cards.prepend(card);
-    closePopup(addPopup); //при нажатии на кнопку "создать" закрываем попап
-  }
+  profileName.textContent = nameInput.value; //в значение имени на странице записываем значение из первого поля
+  profileAbout.textContent = bioInput.value; //в значение био на странице записываем значение из второго поля
+  closePopup(editingProfilePopup); //при нажатии на кнопку "сохранить" закрываем попап
 }
+editingProfilePopup.addEventListener('submit', submitEditProfile);
 
-// Прикрепляем обработчик к форме, он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+// Обработчик «отправки» формы создания карточки
+function submitAddCard(evt) {
+  evt.preventDefault();
+  //добавляем карточку в дерево при нажатии кнопки 'Создать'
+  const card = createCard(titleInput.value, linkInput.value);
+  cardsContainer.prepend(card);
+  closePopup(addingCardPopup); //при нажатии на кнопку "создать" закрываем попап
+}
+addingCardPopup.addEventListener('submit', submitAddCard);
 
-//обходим каждый элемент массива кнопок closeButton
-closeButton.forEach(function (item) {
+//обходим каждый элемент массива кнопок closeButtons
+closeButtons.forEach(function (item) {
   //на каждый элемент вешаем слушатель клика с параметром, содержащим произошедшее событие
   item.addEventListener('click', function (evt) {
-    //удалаяем класс открытого окна у родителя кнопки closeButton
-    evt.target.closest('.popup').classList.remove('popup_opened');
-
+  //удалаяем класс открытого окна у родителя кнопки closeButtons
+  closePopup(evt.target.closest('.popup'));
   });
 });
 
 //функция создания карточки
 function createCard(name, link) {
-  const cardTemplate = document.querySelector('#element-template').content;
   const card = cardTemplate.querySelector('.element').cloneNode(true);
   card.querySelector('.element__title').textContent = name;
   const cardImage = card.querySelector('.element__image');
   cardImage.src = link;
   cardImage.alt = name;
-
 
   //функциональность для лайка карточек
   const likeButton = card.querySelector('.element__like-button');
@@ -125,12 +91,9 @@ function createCard(name, link) {
   });
 
   //функциональность открытия попапа с картинкой
-
   cardImage.addEventListener('click', function (evt) {
-    const openedImage = imagePopup.querySelector('.popup__view-image');
-    openedImage.src = evt.target.src;
-    const openedCaption = imagePopup.querySelector('.popup__view-caption');
-    openedCaption.textContent = evt.target.alt;
+    openedImage.src = link;
+    openedCaption.textContent = name;
     openedImage.alt = openedCaption.textContent;
     openPopup(imagePopup);
   });
@@ -138,12 +101,8 @@ function createCard(name, link) {
   return card;
 }
 
-const cards = document.querySelector('.elements'); //контейнер для всех карточек
-
 //цикл для добавления карточек из массива
 for (let i = 0; i < initialCards.length; i++) {
   let card = createCard(initialCards[i].name, initialCards[i].link);
-  cards.prepend(card);
+  cardsContainer.prepend(card);
 }
-
-
