1383ad053dc928fa1df565abf4d4400ae84b9eb4
Daniella Galitsyna <galitsina@bstec.ru>
2023-02-27 19:25:56 +0100

    add some api function

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,19 +1,25 @@
 import { cardsContainer, hideInputError, validationConfig } from './utils.js';
 import { createCard } from './cards.js';
 import { toggleButtonState } from './validate.js';
+import { editAvatar } from './api.js';
 
 export const editingProfilePopup = document.querySelector('.popup_edit-form');
 export const addingCardPopup = document.querySelector('.popup_add-form');
-const profileName = document.querySelector('.profile__name'); //имя профиля на странице
-const profileAbout = document.querySelector('.profile__bio'); //био профиля на странице
+export const editingAvatarPopup = document.querySelector('.popup_edit-avatar');
+export const avatar = document.querySelector('.profile__avatar');
+export const profileName = document.querySelector('.profile__name'); //имя профиля на странице
+export const profileAbout = document.querySelector('.profile__bio'); //био профиля на странице
 const nameInput = editingProfilePopup.querySelector('#name'); //1 поле редактирования в попапе редактирования
 const bioInput = editingProfilePopup.querySelector('#about'); //2 поле редактирования в попапе редактирования
 const titleInput = addingCardPopup.querySelector('#title'); //1 поле редактирования в попапе создания
 const linkInput = addingCardPopup.querySelector('#link'); //2 поле редактирования в попапе создания
+const avatarLinkInput = editingAvatarPopup.querySelector('#avatarlink'); //поле с ссылкой для изменения аватара
 const editingFormInputs = Array.from(editingProfilePopup.querySelectorAll(validationConfig.inputSelector));
 const editingFormButton = editingProfilePopup.querySelector(validationConfig.submitButtonSelector);
 const addingFormInputs = Array.from(addingCardPopup.querySelectorAll(validationConfig.inputSelector));
 const addingFormButton = addingCardPopup.querySelector(validationConfig.submitButtonSelector);
+const editingAvatarInput = Array.from(editingAvatarPopup.querySelectorAll(validationConfig.inputSelector));
+const editingAvatarButton = editingAvatarPopup.querySelector(validationConfig.submitButtonSelector);
 
 //функциональность открытия окон
 export function openPopup(domElement) {
@@ -55,6 +61,13 @@ export function openAddCard() {
   openPopup(addingCardPopup);
 }
 
+//функциональность открытия окна редактирования аватара
+export function openEditAvatar() {
+  resetFormInPopup(editingAvatarPopup);
+  toggleButtonState(editingAvatarInput, editingAvatarButton, validationConfig); //кнопка становится неактивной при пустых полях
+  openPopup(editingAvatarPopup);
+}
+
 // Обработчик «отправки» формы редактирования профиля
 export function submitEditProfile(evt) {
   // Эта строчка отменяет стандартную отправку формы, так мы можем определить свою логику отправки.
@@ -71,6 +84,27 @@ export function submitAddCard(evt) {
   const card = createCard(titleInput.value, linkInput.value);
   cardsContainer.prepend(card);
   closePopup(addingCardPopup); //при нажатии на кнопку "создать" закрываем попап
+}
+
+//Изменение текста на кнопки при отправке данных на сервер, принимает 2 параметра,т.к нужны 2 разных текста (сохранение, создание)
+function changeButtonText(button, buttonText) {
+  button.textContent = buttonText;
+}
+
+//Обработчик «отправки» формы редактирования аватара
+export function submitEditAvatar(evt) {
+  evt.preventDefault();
+  changeButtonText(editingAvatarButton, 'Сохранение...');
+  editAvatar(avatarLinkInput.value).then((profile) => {
+    avatar.src = profile.avatar;
+  })
+    .catch((err) => {
+      console.log(err);
+    })
+    .finally((res) => {
+      changeButtonText(editingAvatarButton, 'Сохранить');
+    })
+  closePopup(editingAvatarPopup); //при нажатии на кнопку закрываем попап
 }
 
 export function initPopupCloseListeners(closeButtonSelector, popupSelector) {
--- src/index.js
+++ src/index.js
@@ -1,24 +1,106 @@
 import './pages/index.css';
 import { enableValidation } from './components/validate.js';
 import { loadInitialCards } from './components/cards.js';
-import { openAddCard, submitEditProfile, submitAddCard, openEditProfile, initPopupCloseListeners, editingProfilePopup, addingCardPopup } from './components/modal.js';
+import { openAddCard, submitEditProfile, submitAddCard, openEditProfile, initPopupCloseListeners, editingProfilePopup, addingCardPopup, openEditAvatar, submitEditAvatar, editingAvatarPopup, profileName, profileAbout, avatar } from './components/modal.js';
 import { validationConfig } from './components/utils.js';
+import { getUserInformation, getCards, deleteCard, putLike, deleteLike, editAvatar, editProfile, postCard } from './components/api.js';
 
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
+const editAvatarButton = document.querySelector('.profile__avatar-overlay');
 const popupSelector = '.popup';
 const closeButtonSelector = '.popup__close-icon';
+
+getUserInformation()
+  .then ((res) => {
+    profileName.textContent = res.name;
+    profileAbout.textContent = res.about;
+    avatar.src = res.avatar;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 editButton.addEventListener('click', openEditProfile);
 
 addButton.addEventListener('click', openAddCard);
 
+editAvatarButton.addEventListener('click', openEditAvatar);
+
 editingProfilePopup.addEventListener('submit', submitEditProfile);
 
 addingCardPopup.addEventListener('submit', submitAddCard);
+
+editingAvatarPopup.addEventListener('submit', submitEditAvatar);
 
 loadInitialCards();
 
 initPopupCloseListeners(closeButtonSelector, popupSelector);
 
 enableValidation(validationConfig);
+
+function test() {
+  fetch('https://nomoreparties.co/v1/plus-cohort-21/cards', {
+    headers: {
+      authorization: 'ec7ecd53-86da-44fb-8b34-fdbecdd673ff'
+    }
+  })
+    .then(res => res.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getUserInformation()
+  .then(res => {
+    console.log(res)
+  });
+  console.log("Can be befor JSON")
+
+  // editProfile('Dani','Galitsyna')
+  // .then(res => {
+  //   console.log(res)
+  // });
+
+  // postCard('Mermaid', 'https://i2-prod.manchestereveningnews.co.uk/incoming/article16913849.ece/ALTERNATES/s1200b/0_MEN-TRAVEL-COPENHAGEN-mermaid-hoangnt.jpg')
+  // .then(res => {
+  //   console.log(res)
+  // });
+
+  getCards()
+  .then(res => {
+    console.log(res)
+  });
+
+  // deleteCard('63fcad36a606c60d299bf520')
+  // .then(res => {
+  //   console.log(res)
+  // });
+
+  putLike('63fcae4f3093310d1d28ee54')
+  .then(res => {
+    console.log(res)
+  });
+
+  deleteLike('63fcae4f3093310d1d28ee54')
+  .then(res => {
+    console.log(res)
+  });
+
+  putLike('63fcae4f3093310d1d28ee54')
+  .then(res => {
+    console.log(res)
+  });
+
+  putLike('63fcae4f3093310d1d28ee54')
+  .then(res => {
+    console.log(res)
+  });
+
+  // editAvatar('https://i2-prod.manchestereveningnews.co.uk/incoming/article16913849.ece/ALTERNATES/s1200b/0_MEN-TRAVEL-COPENHAGEN-mermaid-hoangnt.jpg')
+  // .then(res => {
+  //   console.log(res)
+  // });
