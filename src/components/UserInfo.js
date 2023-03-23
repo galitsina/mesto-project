fd7584a1e5371482db5e7199bf526b457838e3f6
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2023-03-23 20:25:13 +0300

    fix:class UserInfo - колбэки

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,36 +1,39 @@
 export class UserInfo {
-  constructor(nameSelector, aboutSelector, api) {
+  constructor(nameSelector, aboutSelector, getInfo, setInfo) {
     this.nameSelector = nameSelector;
     this.aboutSelector = aboutSelector;
-    this._api = api;
+    this._getInfo() = getInfo;
+    this._setInfo() = setInfo;
   }
   //публичный метод, который возвращает объект с полученными от сервера данными. Подставить в форму при открытии
   getUserInfo() {
-    return this._api
-      .getUserInformation()
-      .then((res) => {
-        const user = {};
-        user.name = res.name;
-        user.about = res.about;
-        return user;
-      })
-      .catch(err => {
-        console.log(`Ошибка: ${err}`);
-      });
+    return this.getInfo();
   }
 
   //публичный метод setUserInfo, который принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу.
   setUserInfo(name, about) {
-    this._api
-      .editProfile(name, about)
-      .then((res) => {
-        document.querySelector(this.nameSelector).textContent = res.name;
-        document.querySelector(this.aboutSelector).textContent = res.about;
-      })
-      .catch(err => {
-        console.log(`Ошибка: ${err}`);
-      });
+    this._setInfo(name, about, this.nameSelector, this.aboutSelector);
   }
 }
 
-const userInfo = new UserInfo('.profile__name', '.profile__bio', api)
+const userInfo = new UserInfo('.profile__name', '.profile__bio', () => {
+  api.getUserInformation()
+    .then((res) => {
+      const user = {};
+      user.name = res.name;
+      user.about = res.about;
+      return user;
+    })
+    .catch(err => {
+      console.log(`Ошибка: ${err}`);
+    });
+}, (name, about, nameSelector, aboutSelector) => {
+  api.editProfile(name, about)
+    .then((res) => {
+      document.querySelector(nameSelector).textContent = res.name;
+      document.querySelector(aboutSelector).textContent = res.about;
+    })
+    .catch(err => {
+      console.log(`Ошибка: ${err}`);
+    });
+})
