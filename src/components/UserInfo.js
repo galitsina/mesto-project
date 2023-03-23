27ab5e7299a08dd5454ee8190167ae26749777e0
Tanya Katkova <yndx-katkovatanya@yandex.ru>
2023-03-23 17:35:16 +0300

    fix: class UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,25 +1,36 @@
 export class UserInfo {
-  constructor(nameSelector, aboutSelector) {
-    this.nameSelector = nameSelector
-    this.aboutSelector = aboutSelector
+  constructor(nameSelector, aboutSelector, api) {
+    this.nameSelector = nameSelector;
+    this.aboutSelector = aboutSelector;
+    this._api = api;
   }
   //публичный метод, который возвращает объект с полученными от сервера данными. Подставить в форму при открытии
   getUserInfo() {
-    return api.getUserInformation()
+    return this._api
+      .getUserInfo()
       .then((res) => {
         const user = {};
         user.name = res.name;
-        user.info = res.about;
+        user.about = res.about;
         return user;
       })
+      .catch(err => {
+        console.log(`Ошибка: ${err}`);
+      });
   }
 
   //публичный метод setUserInfo, который принимает новые данные пользователя, отправляет их на сервер и добавляет их на страницу.
   setUserInfo(name, about) {
-    api.editProfile(name, about)
+    this._api
+      .editProfile(name, about)
       .then((res) => {
-        this.nameSelector.textContent = res.name;
-        this.aboutSelector.textContent = res.about;
+        document.querySelector(this.nameSelector).textContent = res.name;
+        document.querySelector(this.aboutSelector).textContent = res.about;
+      })
+      .catch(err => {
+        console.log(`Ошибка: ${err}`);
       });
   }
 }
+
+const UserInfo = new UserInfo('.profile__name', '.profile__bio', api)
